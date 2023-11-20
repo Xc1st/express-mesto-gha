91f619d5896d6ba0918fd6786f00f0d80e929247
Daniil <dan.ustinoff2011@yandex.ru>
2023-11-20 21:33:27 +0300

    test14

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const { errors } = require('celebrate');
+const handleError = require('./middlewares/handle-error');
 
 const { PORT = 3000, URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+app.use(helmet());
+
+app.use(limiter);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,14 +26,9 @@ mongoose.connect(URL, {
   useUnifiedTopology: true,
 });
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '654ca0c78401b8a363a1eed2',
-  };
-  next();
-});
-
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
+app.use(errors());
+app.use(handleError);
 app.listen(PORT);
--- controllers/cards.js
+++ controllers/cards.js
@@ -1,86 +1,103 @@
+const { HTTP_STATUS_OK, HTTP_STATUS_CREATED } = require('http2').constants;
 const mongoose = require('mongoose');
 const Card = require('../models/card');
+const NotFoundError = require('../errors/NotFoundError');
+const BadRequestError = require('../errors/BadRequestError');
+const ForbiddenError = require('../errors/ForbiddenError');
 
-module.exports.addCard = (req, res) => {
+module.exports.addCard = (req, res, next) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((card) => {
       Card.findById(card._id)
         .orFail()
         .populate('owner')
-        .then((data) => res.status(201).send(data))
+        .then((data) => res.status(HTTP_STATUS_CREATED).send(data))
         .catch((error) => {
           if (error instanceof mongoose.Error.DocumentNotFoundError) {
-            res.status(404).send({ message: 'Карточка с данным _id не найдена' });
+            next(new NotFoundError('Карточка с данным _id не найдена'));
           } else {
-            res.status(500).send({ message: 'На сервере произошла ошибка' });
+            next(error);
           }
         });
     })
     .catch((error) => {
       if (error instanceof mongoose.Error.ValidationError) {
-        res.status(400).send({ message: error.message });
+        next(new BadRequestError(error.message));
       } else {
-        res.status(500).send({ message: 'На сервере произошла ошибка' });
+        next(error);
       }
     });
 };
 
-module.exports.getCards = (req, res) => {
+module.exports.getCards = (req, res, next) => {
   Card.find({})
     .populate(['owner', 'likes'])
     .then((cards) => res.send(cards))
-    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+    .catch(next);
 };
 
-module.exports.deleteCard = (req, res) => {
-  Card.findByIdAndDelete(req.params.cardId)
-    .orFail()
-    .then(() => {
-      res.status(200).send({ message: 'Карточка удалена' });
+module.exports.deleteCard = (req, res, next) => {
+  Card.findById(req.params.cardId)
+    .then((card) => {
+      if (!card.owner.equals(req.user._id)) {
+        throw new ForbiddenError('Карточка другого пользователя');
+      }
+      Card.deleteOne(card)
+        .orFail()
+        .then(() => {
+          res.status(HTTP_STATUS_OK).send({ message: 'Карточка удалена' });
+        })
+        .catch((error) => {
+          if (error instanceof mongoose.Error.CastError) {
+            next(new BadRequestError('неправильный _id'));
+          } else if (error instanceof mongoose.Error.DocumentNotFoundError) {
+            next(new NotFoundError('Карточка с данным  _id не найдена.'));
+          } else {
+            next(error);
+          }
+        });
     })
     .catch((error) => {
-      if (error instanceof mongoose.Error.CastError) {
-        res.status(400).send({ message: 'неправильный _id' });
-      } else if (error instanceof mongoose.Error.DocumentNotFoundError) {
-        res.status(404).send({ message: 'Карточка с данным  _id не найдена.' });
+      if (error.name === 'TypeError') {
+        next(new NotFoundError('Карточка с данным  _id не найдена.'));
       } else {
-        res.status(500).send({ message: 'На сервере произошла ошибка' });
+        next(error);
       }
     });
 };
 
-module.exports.likeCard = (req, res) => {
+module.exports.likeCard = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
     .orFail()
     .populate(['owner', 'likes'])
     .then((card) => {
-      res.send(card);
+      res.status(HTTP_STATUS_OK).res.send(card);
     })
     .catch((error) => {
       if (error instanceof mongoose.Error.DocumentNotFoundError) {
-        res.status(404).send({ message: 'Карточка с данным _id не найдена' });
+        next(new NotFoundError('Карточка с данным  _id не найдена.'));
       } else if (error instanceof mongoose.Error.CastError) {
-        res.status(400).send({ message: 'неправильный _id' });
+        next(new BadRequestError('неправильный _id'));
       } else {
-        res.status(500).send({ message: 'На сервере произошла ошибка' });
+        next(error);
       }
     });
 };
-module.exports.dislikeCard = (req, res) => {
+module.exports.dislikeCard = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
     .orFail()
     .populate(['owner', 'likes'])
     .then((card) => {
-      res.send(card);
+      res.status(HTTP_STATUS_OK).res.send(card);
     })
     .catch((error) => {
       if (error instanceof mongoose.Error.DocumentNotFoundError) {
-        res.status(404).send({ message: 'Карточка с данным _id не найдена' });
+        next(new NotFoundError('Карточка с данным  _id не найдена.'));
       } else if (error instanceof mongoose.Error.CastError) {
-        res.status(400).send({ message: 'неправильный _id' });
+        next(new BadRequestError('неправильный _id'));
       } else {
-        res.status(500).send({ message: 'На сервере произошла ошибка' });
+        next(error);
       }
     });
 };
